0e882af12e362aabaeee1e2f4ff637ae66de6028
vbas <vbas@cin.ufpe.br>
2019-11-26 21:02:48 -0300

    adding google login button to the screen

--- app/src/main/java/com/ufpe/onepercent/MainActivity.kt
+++ app/src/main/java/com/ufpe/onepercent/MainActivity.kt
@@ -2,11 +2,27 @@ package com.ufpe.onepercent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
     }
 }
