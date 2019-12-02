081aaa2a90f9242dcfc4a6162dc45651a354aa89
vbas <vbas@cin.ufpe.br>
2019-12-01 23:07:32 -0300

    map not working (maybe google play services import)

--- app/src/main/java/com/ufpe/onepercent/MapActivity.kt
+++ app/src/main/java/com/ufpe/onepercent/MapActivity.kt
@@ -2,14 +2,25 @@ package com.ufpe.onepercent
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 
 class MapActivity : AppCompatActivity() {
+
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
         // https://www.youtube.com/watch?v=suwq7Nta3oM
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+        })
     }
 
 }
